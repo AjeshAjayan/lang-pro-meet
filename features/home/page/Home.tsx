42c8986eeb372c8fff0f7c301e90e9571744e64b
Ajesh Ajayan <ajeshajayan8893@gmail.com>
2024-03-28 21:00:36 +0530

    clear state on unmound

--- features/home/page/Home.tsx
+++ features/home/page/Home.tsx
@@ -1,15 +1,21 @@
 import { View, Text, StyleSheet } from 'react-native';
 import { Button } from 'react-native-paper';
 import { LoginStore } from '../store/store';
+import { useEffect } from 'react';
+import { defaultCounterState } from '../store/counterState';
 
 export const Home = () => {
 
     const count = LoginStore.useState(l => l.count);
 
+    useEffect(() => () => {
+        LoginStore.update(l => defaultCounterState())
+    }, [])
+
     return <View style={styles.container}>
         <Button onPress={() => {
             LoginStore.update(l => {
-                l.count++                   
+                l.count++
             })
         }}> Click me </Button>
         <Text>{count}</Text>
