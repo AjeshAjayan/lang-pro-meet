b161254aeb3be8d898be7bae2f402b9a036d74c1
Ajesh Ajayan <ajeshajayan8893@gmail.com>
2024-03-28 17:30:55 +0530

    Log in UI

--- App.tsx
+++ App.tsx
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, PaperProvider, TextInput } from 'react-native-paper';
+import { theme } from './features/theme';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          mode="outlined"
+          label="e-mail/phone"
+          placeholder="Type sromething"
+          right={<TextInput.Icon icon="account-circle" />}
+        />
+        <TextInput
+          style={styles.input}
+          mode="outlined"
+          label="password"
+          secureTextEntry
+          placeholder="Type sromething"
+          right={<TextInput.Icon icon="lock" />}
+        />
+        <Button style={styles.button} icon="login" mode="contained" onPress={() => console.log('Pressed')}>
+          Log In
+        </Button>
+      </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    gap: 10,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 26
   },
+  input: {
+    width: '100%'
+  },
+  button: {
+    width: '100%',
+    borderRadius: 4
+  }
 });
